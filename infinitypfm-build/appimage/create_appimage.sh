838d9cf7740995a177a342edd5cf1acbd0926797
Wayne Gray <woodward186@tuxfor.me>
2024-01-02 19:44:48 -0500

    Use Jlink to reduce size of appImage

--- infinitypfm-build/appimage/create_appimage.sh
+++ infinitypfm-build/appimage/create_appimage.sh
@@ -5,7 +5,8 @@ Enter release version to use when naming the app image: \n"
 read response
 
 chmod a+x infinitypfm.AppDir/AppRun;
-cp -L -R /usr/lib/jvm/java-17-openjdk-amd64 infinitypfm.AppDir/usr/lib/;
+#cp -L -R /usr/lib/jvm/java-17-openjdk-amd64 infinitypfm.AppDir/usr/lib/;
+jlink --add-modules java.base,java.naming,java.desktop,java.sql,java.datatransfer,java.logging --output infinitypfm.AppDir/usr/lib/jre --compress=2 --no-header-files --no-man-pages --strip-debug
 
 #cp infinitypfm.run infinitypfm.AppDir/usr/bin/infinitypfm
 
